143e8f2b96a0d484f3a840550ca892493687f638
Zura Chanishvili <zurachanishvili@Zuras-MacBook-Pro.local>
2023-05-23 13:07:09 +0400

    added functionaliti search , new entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>/", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("new_entry/", views.new_entry, name="new_entry"),
+    path("edit_entry/", views.edit_entry, name="edit_entry"),
 ]
+ 
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,82 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
+
+
+lists=util.list_entries()
+
+
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def entry(request,title):
+    
+    if util.get_entry(title) == None:
+         return render(request, "encyclopedia/error.html", {
+            'message': f'Error: [{title}] - Entry not found'
+        })
+    else:     
+        return render(request, "encyclopedia/entry.html", {
+            'title':title,
+            'entry': markdowner.convert(util.get_entry(title)) 
+        })
+        
+        
+def search(request):
+    if request.method == 'POST':
+        entry_search = request.POST['q']
+        entries = util.list_entries()
+        match_entries = [entry for entry in entries if entry_search.lower()in entry.lower()]
+        if util.get_entry(entry_search) is not None:
+            return render(request, "encyclopedia/entry.html", {
+                'title':entry_search,
+                'entry': markdowner.convert(util.get_entry(entry_search)) 
+            })
+        elif match_entries:
+            return render(request, 'encyclopedia/search.html', {
+                'entries' : match_entries,
+                'entry_search' : entry_search
+            })
+        else:
+            return render(request, "encyclopedia/error.html", {
+                    'message': f'No results found for : {entry_search}'
+            })
+    
+
+# function to make new entry it should take title and content and use save_entry(title, content)
+
+
+def new_entry(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        if util.get_entry(title) is not None:
+            return render(request, "encyclopedia/error.html", {
+                'message': f'Error: [{title}] - Entry already exists'
+            })
+        else:
+            util.save_entry(title,content)
+            return render(request, "encyclopedia/entry.html", {
+                'title':title,
+                'entry': markdowner.convert(util.get_entry(title)) 
+            })
+    else:
+        return render(request, "encyclopedia/new_entry.html")        
+    
+    
+def edit_entry(request,title):
+    if util.get_entry(title) == None:
+         return render(request, "encyclopedia/error.html", {
+            'message': f'Error: [{title}] - Entry not found'
+        })
+    else:     
+        return render(request, "encyclopedia/edit_entry.html", {
+            'title':title,
+            'entry': markdowner.convert(util.get_entry(title)) 
+        })   
